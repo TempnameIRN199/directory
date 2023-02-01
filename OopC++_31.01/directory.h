50489c478dc0e96142b8d318154b2db6bb9055ea
考え-Sueta <vityayuzbekov602@gmail.com>
2023-02-01 16:02:44 +0200

    .

--- OopC++_31.01/directory.h
+++ OopC++_31.01/directory.h
@@ -24,23 +24,13 @@ public:
 	string getAddress();
 	string getActivity();
 	void showMenu();
-	// создать файл для записи
 	void createFile();
-	// вывод всех данных из файла
 	void showAll();
-	// запись данных в файл
 	void writeToFile();
-	// чтение данных из файла
 	void readFromFile();
-	// Поиск по названию
 	void searchName();
-	// Поиск по владельцу
 	void searchOwner();
-	// Поиск по номеру телефона
 	void searchPhone();
-	// Поиск по роду деятельности
 	void searchActivity();
-	// добавление записи ранее созданного файла
-	void addRecord();
 	void changeRecord();
 };
